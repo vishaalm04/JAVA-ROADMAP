9c452ae7b7a33353672de0963d629eded4f143a3
vishaalm04 <vishaal.se21@bitsathy.ac.in>
2025-02-05 16:40:20 +0530

    test 1

---
+++ src/if_else.java
@@ -0,0 +1,14 @@
+public class if_else {
+
+        public static void main(String[] args) {
+            int number = 10;
+
+            if (number % 2 == 0) {
+                System.out.println(number + " is Even");
+            } else {
+                System.out.println(number + " is Odd");
+            }
+        }
+    }
+
+
