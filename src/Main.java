ee6e430649df2f44b277352f86d8ff31395e4db3
vishaalm04 <vishaal.se21@bitsathy.ac.in>
2025-02-05 16:26:03 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte byteVar = 127;
+        short shortVar = 32000;
+        int intVar = 100000;
+        long longVar = 1000000000L;
+
+        float floatVar = 10.5f;
+        double doubleVar = 99.99;
+
+        char charVar = 'A';
+        boolean boolVar = true;
+
+
+        String str = "Hello, Java!";
+
+
+        System.out.println("Byte: " + byteVar);
+        System.out.println("Short: " + shortVar);
+        System.out.println("Int: " + intVar);
+        System.out.println("Long: " + longVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Char: " + charVar);
+        System.out.println("Boolean: " + boolVar);
+        System.out.println("String: " + str);
+
+    }
+}
